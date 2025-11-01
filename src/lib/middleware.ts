944d710841b7aeb85308644237562e4fb06def41
tanahiro2010 <herentongkegu087@gmail.com>
2025-11-01 18:15:53 +0900

    edit:

--- src/lib/middleware.ts
+++ src/lib/middleware.ts
@@ -2,6 +2,7 @@ import type { Session } from "@/types/session";
 import { NextRequest } from "next/server";
 import { verifyJWT } from "@/utils/token";
 import { auth } from "./auth";
+import { unauthorizedResponse } from "../utils/response";
 
 // ハンドラー関数のオーバーロード定義
 type AuthHandler = {
@@ -11,6 +12,7 @@ type AuthHandler = {
 
 export async function withAuth(req: NextRequest, handler: AuthHandler) {
     const token = req.headers.get("Authorization")?.replace("Bearer ", "");
+    
     if (!token) {
         const session = await auth.api.getSession({
             headers: req.headers,
@@ -18,11 +20,14 @@ export async function withAuth(req: NextRequest, handler: AuthHandler) {
         if (session) {
             return handler(req, "session", session);
         }
+        return unauthorizedResponse();
     }
+    
     // トークンの検証
-    const payload = await verifyJWT(token!);
+    const payload = await verifyJWT(token);
     if (!payload) {
-        return new Response(JSON.stringify({ error: "無効なトークンです" }), { status: 401 });
+        return unauthorizedResponse("無効なトークンです");
     }
+    
     return handler(req, "token", payload);
 }
