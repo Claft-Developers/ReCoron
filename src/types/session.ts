5f55b1c00bc417745715568dd01d02d41acccc5a
tanahiro2010 <herentongkegu087@gmail.com>
2025-11-01 17:09:56 +0900

    add: session type

---
+++ src/types/session.ts
@@ -0,0 +1,23 @@
+type Session = {
+    session: {
+        id: string;
+        createdAt: Date;
+        updatedAt: Date;
+        userId: string;
+        expiresAt: Date;
+        token: string;
+        ipAddress?: string | null | undefined;
+        userAgent?: string | null | undefined;
+    };
+    user: {
+        id: string;
+        createdAt: Date;
+        updatedAt: Date;
+        email: string;
+        emailVerified: boolean;
+        name: string;
+        image?: string | null | undefined;
+    };
+};
+
+export { type Session };
