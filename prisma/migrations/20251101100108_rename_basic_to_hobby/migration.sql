9ca90bac8a51eb99410e4eada4f4f345591a225a
tanahiro2010 <herentongkegu087@gmail.com>
2025-11-01 19:34:15 +0900

    fix: typo plan name

---
+++ prisma/migrations/20251101100108_rename_basic_to_hobby/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - The values [BASIC] on the enum `Plan` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "Plan_new" AS ENUM ('FREE', 'HOBBY', 'PRO');
+ALTER TABLE "public"."user" ALTER COLUMN "plan" DROP DEFAULT;
+ALTER TABLE "user" ALTER COLUMN "plan" TYPE "Plan_new" USING ("plan"::text::"Plan_new");
+ALTER TYPE "Plan" RENAME TO "Plan_old";
+ALTER TYPE "Plan_new" RENAME TO "Plan";
+DROP TYPE "public"."Plan_old";
+ALTER TABLE "user" ALTER COLUMN "plan" SET DEFAULT 'FREE';
+COMMIT;
