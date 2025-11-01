861856fba4f1b782101036388576e5c0c93ce02f
tanahiro2010 <herentongkegu087@gmail.com>
2025-11-01 17:09:43 +0900

    add: auth middleware

---
+++ src/lib/middleware.ts
@@ -0,0 +1,28 @@
+import type { Session } from "@/types/session";
+import { NextRequest } from "next/server";
+import { verifyJWT } from "@/utils/token";
+import { auth } from "./auth";
+
+// ハンドラー関数のオーバーロード定義
+type AuthHandler = {
+    (req: NextRequest, type: "session", payload: Session): Promise<Response>;
+    (req: NextRequest, type: "token", payload: Record<string, any>): Promise<Response>;
+};
+
+export async function withAuth(req: NextRequest, handler: AuthHandler) {
+    const token = req.headers.get("Authorization")?.replace("Bearer ", "");
+    if (!token) {
+        const session = await auth.api.getSession({
+            headers: req.headers,
+        });
+        if (session) {
+            return handler(req, "session", session);
+        }
+    }
+    // トークンの検証
+    const payload = await verifyJWT(token!);
+    if (!payload) {
+        return new Response(JSON.stringify({ error: "無効なトークンです" }), { status: 401 });
+    }
+    return handler(req, "token", payload);
+}
