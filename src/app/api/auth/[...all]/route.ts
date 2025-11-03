a20e76c3e7b0ea09fe5d1fc22e3b014b30269a53
tanahiro2010 <herentongkegu087@gmail.com>
2025-11-03 20:22:33 +0900

    remove: log for debug

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -2,7 +2,5 @@ import { auth } from "@/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
 
 // デバッグ: auth オブジェクトの確認
-console.log("Auth handler initialized:", !!auth);
-console.log("Available routes:", Object.keys(auth.api || {}));
 
 export const { POST, GET } = toNextJsHandler(auth);
