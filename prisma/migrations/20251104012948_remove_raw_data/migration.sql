d036831b913f191462d00e9cb7ee6dd0d7577e67
tanahiro2010 <herentongkegu087@gmail.com>
2025-11-04 10:30:17 +0900

    sync: prisma database

---
+++ prisma/migrations/20251104012948_remove_raw_data/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `job_raw` on the `RunningLog` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "RunningLog" DROP COLUMN "job_raw";
