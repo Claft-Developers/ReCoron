81ce64b295945b8cb6b9780b50654b371d3bdced
tanahiro2010 <herentongkegu087@gmail.com>
2025-11-05 18:22:59 +0900

    add: docs redirect setting

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/docs/:slug.md",
+        destination: "/docs/:slug",
+        permanent: true,
+      }
+    ]
+  },
 };
 
 export default nextConfig;
